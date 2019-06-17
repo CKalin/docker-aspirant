cfa91fad0b25c27cfacd482679a712c9eb630bf1
Marco Klein <marco.klein@avsn.de>
2019-06-17 14:27:06 +0200

    little styling.

--- public_html/index.php
+++ public_html/index.php
@@ -13,14 +13,18 @@
       </a>
       <div  class="speechbubble-end"></div>
     </header>
-    This is a blank page! But you can use the public_html folder to serve static files like single-page apps or whatever else you want.
+    <main>
+      <div class="main-box">
+        This is a blank page! But you can use the public_html folder to serve static files like single-page apps or whatever else you want.
 
-    <p>
-      You are calling from IP Address: <?php echo $_SERVER['REMOTE_ADDR'] ?>
-    </p>
-    <p>
-      The Reviews API is hosted at:
-      <?php echo get_reviews_api_url(); ?>
-    </p>
+        <p>
+          You are calling from IP Address: <?php echo $_SERVER['REMOTE_ADDR'] ?>
+        </p>
+        <p>
+          The Reviews API is hosted at:
+          <?php echo get_reviews_api_url(); ?>
+        </p>
+      </div>
+    </main>
   </body>
 </html>
